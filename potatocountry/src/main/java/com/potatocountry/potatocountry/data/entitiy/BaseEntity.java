b68e10964795e26bca21e7017744a9b00619585c
taehyeon <kth0373@naver.com>
2025-02-11 18:59:46 +0900

    ✨ [Feature] checkStyle 수정

--- potatocountry/src/main/java/com/potatocountry/potatocountry/data/entitiy/BaseEntity.java
+++ potatocountry/src/main/java/com/potatocountry/potatocountry/data/entitiy/BaseEntity.java
@@ -6,8 +6,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
--- potatocountry/src/main/java/com/potatocountry/potatocountry/data/entitiy/User.java
+++ potatocountry/src/main/java/com/potatocountry/potatocountry/data/entitiy/User.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User extends BaseEntity{
+public class User extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
